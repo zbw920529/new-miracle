1451c023f8293bb82566ffb263f0e752c6168590
zbw920529 <41557219+zbw920529@users.noreply.github.com>
2019-01-28 14:43:53 +0800

    PC端开发

---
+++ precache-manifest.7f52b4620dc79eba19ca7a66eedcb0dd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "601b7b1914ec28d566a2f1c79c648d8f",
+    "url": "/static/media/znjc.601b7b19.gif"
+  },
+  {
+    "revision": "bf45978dd1e72ffa9808",
+    "url": "/static/css/main.f77de96d.chunk.css"
+  },
+  {
+    "revision": "4c3b2a3ff3bdc24040af",
+    "url": "/static/js/1.4c3b2a3f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1c82ee0004505530cfdfe8269f9d587",
+    "url": "/static/media/banner_gd.d1c82ee0.png"
+  },
+  {
+    "revision": "aeb072b409dacf566b0e057e97adc629",
+    "url": "/static/media/banner_tm.aeb072b4.png"
+  },
+  {
+    "revision": "94c8d3aea032b1c7fd25a4624e4006fa",
+    "url": "/static/media/banner_gmi.94c8d3ae.jpg"
+  },
+  {
+    "revision": "bf45978dd1e72ffa9808",
+    "url": "/static/js/main.bf45978d.chunk.js"
+  },
+  {
+    "revision": "20e07ba305e820974f9b5c0252ad9cca",
+    "url": "/static/media/znph.20e07ba3.gif"
+  },
+  {
+    "revision": "edd0ad5eef3dca28ec3e76f67a2dca56",
+    "url": "/static/media/image_cyhz.edd0ad5e.jpg"
+  },
+  {
+    "revision": "47bb76b8daa6ef5c0eb201e558d6f5b6",
+    "url": "/static/media/btn_yge2.47bb76b8.jpg"
+  },
+  {
+    "revision": "4aa6a2c39eebb4c434569642fdfa6dc0",
+    "url": "/static/media/image_db.4aa6a2c3.png"
+  },
+  {
+    "revision": "9d89d3dff4885af87b88bd8403f67075",
+    "url": "/static/media/bg_3.9d89d3df.jpg"
+  },
+  {
+    "revision": "4740faee1c5d6c8a69c4b40585d1e6d1",
+    "url": "/index.html"
+  }
+];

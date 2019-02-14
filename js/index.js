aa2cfb8a6d86fea66e65d0a6ca5e0ab9f860a7c4
zbw920529 <41557219+zbw920529@users.noreply.github.com>
2019-02-14 12:18:55 +0800

    修改导航选中效果和合作伙伴图片切换

--- js/index.js
+++ js/index.js
@@ -1,50 +1,76 @@
-var nav=document.getElementById('nav')
-var li=document.getElementsByClassName('nav-li')
 var znph=document.getElementById('znph')
 var znjc=document.getElementById('znjc')
 var gif=document.getElementById('gif')
-var index=0
-var checkd=0
-var navTitle=[
-    {title:'首页',url:'#banner',name:'/'},
-    {title:'关于我们',url:'#about',name:'about'},
-    {title:'合作伙伴',url:'#panner',name:'about'},
-    {title:'联系我们',url:'#footer',name:'about'},
-]
-var length=navTitle.length
-navs()
-function navs(){
-    var html=''
-
-    for(var i=0;i<length;i++){
-        // var navClass=index===i?'a active-nav':'' class="+navClass+'a'+"
-        console.log(index===i)
-        html+= "<li class='nav-li' onclick='changIndexNav(i)' >"+ "<a href="+navTitle[i].url+" class='a' >" +navTitle[i].title+ "</a>"+ "</li>"
+var nre=document.getElementById('nre')
+var lsbe=document.getElementById('lsbe')
+var yge=document.getElementById('yge')
+var tpn=document.getElementById('tpn')
+// 导航栏选中效果
+function changeActive(navIndex){
+    var navHome=document.getElementById('nav-home')
+    var navAbout=document.getElementById('nav-about')
+    var navPanner=document.getElementById('nav-panner')
+    var navContact=document.getElementById('nav-contact')
+    switch(navIndex){
+        case 'navHome':
+            return(
+                navHome.className='active',navAbout.className='a',navPanner.className='a',navContact.className='a'
+            )
+        case 'navAbout':
+            return(
+                navHome.className='a',navAbout.className='active',navPanner.className='a',navContact.className='a'
+            )
+        case 'navPanner':
+            return(
+                navHome.className='a',navAbout.className='a',navPanner.className='active',navContact.className='a'
+            )
+        case 'navContact':
+            return(
+                navHome.className='a',navAbout.className='a',navPanner.className='a',navContact.className='active'
+            )
+        default:
+            return(
+                navHome.class='active',navAbout.class='a',navPanner.class='a',navContact.class='a'
+            )
     }
-    nav.innerHTML=html
 }
-function changIndexNav(a) {
-    console.log(a)
-}
-
-for(i=0;i<li.length;i++){
-    li[i].onclick=function(){
-        for(j=0;j<li.length;j++){
-            li[j].className="a";
-        }
-        this.className="active-nav";
-    }
-}
+// 关于我们右边图片切换
 function changeCheckd(index){
     if(index===0){
         znph.src='./img/btn_znph1.jpg'
         znjc.src='./img/btn_znjc1.jpg'
         gif.src='./img/znph.gif'
-        console.log(znph)
     }else{
         znph.src='./img/btn_znph2.jpg'
         znjc.src='./img/btn_znjc2.jpg'
         gif.src='./img/znjc.gif'
-        console.log(index)
+    }
+}
+//合作伙伴图片切换
+function pannerImg(value){
+    switch(value){
+        case 'nre':
+            return(
+                nre.src='./img/btn_nre2.jpg',lsbe.src='./img/btn_lsbe1.jpg',yge.src='./img/btn_yge1.jpg',tpn.src='./img/btn_tpn1.jpg'
+
+            )
+        case 'lsbe':
+            return(
+                nre.src='./img/btn_nre1.jpg',lsbe.src='./img/btn_lsbe2.jpg',yge.src='./img/btn_yge1.jpg',tpn.src='./img/btn_tpn1.jpg')
+        case 'yge':
+            return(
+                nre.src='./img/btn_nre1.jpg',lsbe.src='./img/btn_lsbe1.jpg',yge.src='./img/btn_yge2.jpg',tpn.src='./img/btn_tpn1.jpg'
+
+            )
+        case 'tpn':
+            return(
+                nre.src='./img/btn_nre1.jpg',lsbe.src='./img/btn_lsbe1.jpg',yge.src='./img/btn_yge1.jpg',tpn.src='./img/btn_tpn2.jpg'
+
+            )
+        default:
+            return(
+                nre.src='./img/btn_nre1.jpg',lsbe.src='./img/btn_lsbe1.jpg',yge.src='./img/btn_yge1.jpg',tpn.src='./img/btn_tpn2.jpg'
+
+            )
     }
 }

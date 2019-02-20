6235b768d2047f819c57d664cc03f5c1c3b05361
zbw920529 <41557219+zbw920529@users.noreply.github.com>
2019-02-20 12:00:27 +0800

    新增弹出框

--- js/index.js
+++ js/index.js
@@ -6,6 +6,38 @@ var lsbe=document.getElementById('lsbe')
 var yge=document.getElementById('yge')
 var tpn=document.getElementById('tpn')
 // 导航栏选中效果
+//锚点滑动效果
+$(function(){
+    //锚点跳转滑动效果
+    $('a[href*=#],area[href*=#]').click(function() {
+        console.log(this.pathname)
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({
+                        scrollTop: targetOffset
+                    },
+                    300);
+                return false;
+            }
+        }
+    });
+})
+
+var clientWidth = document.documentElement.clientWidth || document.body.offsetWidth;
+// 获取根元素html
+var html = document.documentElement;
+//获取当前视口宽度和标准宽度比例 * 标准rem单位得到html的font-size这里设置的行内样式权重最大
+html.style.fontSize = clientWidth/1920*100 + "px";
+// 监听window的onresize屏幕的尺寸变化
+// 如果我们改变为横屏时，html的font-size发生变化
+window.onresize = function () {
+    var clientWidth = document.documentElement.clientWidth || document.body.offsetWidth;
+    //获取当前视口宽度和标准宽度比例 * 标准rem单位 1rem=512px
+    document.documentElement.style.fontSize = clientWidth/1920*100 + "px";
+};
 function changeActive(navIndex){
     var navHome=document.getElementById('nav-home')
     var navAbout=document.getElementById('nav-about')
@@ -69,8 +101,11 @@ function pannerImg(value){
             )
         default:
             return(
-                nre.src='./img/btn_nre1.jpg',lsbe.src='./img/btn_lsbe1.jpg',yge.src='./img/btn_yge1.jpg',tpn.src='./img/btn_tpn2.jpg'
+                nre.src='./img/btn_nre1.jpg',lsbe.src='./img/btn_lsbe1.jpg',yge.src='./img/btn_yge1.jpg',tpn.src='./img/btn_tpn1.jpg'
 
             )
     }
+}
+function pannerLeaveImg(){
+    nre.src='./img/btn_nre1.jpg',lsbe.src='./img/btn_lsbe1.jpg',yge.src='./img/btn_yge1.jpg',tpn.src='./img/btn_tpn1.jpg'
 }
